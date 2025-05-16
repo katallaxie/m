2d929b6e517e954b05ca7b82cc197cbaadf961a4
Sebastian Döll <sebastian@katallaxie.dev>
2025-05-16 19:05:13 +0000

    wip: add chat message

--- internal/store/actions.go
+++ internal/store/actions.go
@@ -11,6 +11,22 @@ type AddChatMsg struct {
 	Chat *models.Chat
 }
 
+// AddChatMessageMsg ...
+type AddChatMessageMsg struct {
+	ChatID  uuid.UUID
+	Message *models.Message
+}
+
+// NewAddChatMessage ...
+func NewAddChatMessage(chatID uuid.UUID, message *models.Message) func() redux.Update {
+	return func() redux.Update {
+		return &AddChatMessageMsg{
+			ChatID:  chatID,
+			Message: message,
+		}
+	}
+}
+
 // NewAddChat ...
 func NewAddChat(chat *models.Chat) func() redux.Update {
 	return func() redux.Update {
