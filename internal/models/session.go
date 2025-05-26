745f0afb3156d3d95ebaad77b913db51091d958d
Sebastian Döll <sebastian@katallaxie.dev>
2025-05-26 14:02:57 +0000

    chore: add sqlite viewer

--- internal/models/session.go
+++ internal/models/session.go
@@ -1,11 +1,22 @@
 package models
 
-import "github.com/google/uuid"
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
 
 // Session is a struct that represents a session in the application.
 type Session struct {
 	// ID is the primary key
-	ID uuid.UUID `json:"id" gorm:"type:uuid;primary_key;default:gen_random_uuid()" params:"id"`
+	ID uuid.UUID `json:"id" gorm:"type:uuid;primary_key"`
 	// Name is the name of the session.
-	Name string `json:"name" gorm:"type:varchar(255);not null;default:'Untitled'" params:"name"`
+	Name string `json:"name" gorm:"type:varchar(255);not null;default:'Untitled'"`
+	// CreatedAt ...
+	CreatedAt time.Time `json:"created_at"`
+	// UpdatedAt ...
+	UpdatedAt time.Time `json:"updated_at"`
+	// DeletedAt ...
+	DeletedAt gorm.DeletedAt `json:"deleted_at"`
 }
